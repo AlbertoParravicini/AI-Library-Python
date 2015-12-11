06a647493cefe7980c02596247039c6b52674e98
Alberto Parravicini <albyratta@gmail.com>
2015-12-11 19:38:51 +0100

    More documentation

--- AI-Library/Adversarial Search Engines/Minimax.py
+++ AI-Library/Adversarial Search Engines/Minimax.py
@@ -2,13 +2,27 @@ from AdversarialSearchEngine import AdversarialSearchEngine
 import random as random
 
 class Minimax(AdversarialSearchEngine):
+    """
+    Implementation of the standard Minimax algorithm.
+    The algorithm works for zero-sum, two-players, turn-based games, with perfect knowledge and deterministic moves.
+    Given an initial node, it will look for the best move that the current player can perform,
+    under the assumption that both players will play rationally (i.e optimally);
+    """
     
     def __init__(self, problem, search_depth = 1):
         super().__init__(problem, search_depth)
     
-    def perform_search(self, initial_node):
-        
+    def perform_search(self, initial_node):  
         """
+        Perform a search from the provided initial_node, by using the rules
+        expressed in the associated problem.
+        The result of the search will be stored in obtained_result and obtained_successor.
+        If the initial_node has no successors, the obtained_successor will be None,
+        and obtained_result will be calculated on the initial_node;
+
+        Parameters:
+        -------------
+        initial_node: the node from which the search starts;
         """
         
         self.obtained_successor = None
@@ -16,15 +30,20 @@ class Minimax(AdversarialSearchEngine):
         curr_depth = 0
 
         for curr_succ in self.problem.get_successors(initial_node):
+            # If the maximum depth is set to 0, return a random successor node;
             if self.search_depth == 0:
                 self.obtained_successor = random.choice(self.problem.get_successors(initial_node))
                 self.obtained_value = self.problem.value(self.obtained_successor)
                 break
+            
+            # A certain player might play more than one turn in a row, 
+            # so no assumptions are made with respect to the turn alternation;
             if curr_succ.is_max():
                 result = self.__max(curr_succ, curr_depth + 1)
             else:
                 result = self.__min(curr_succ, curr_depth + 1)
 
+            # If a new best move was found, save it along with the value provided by the search;
             if (initial_node.is_max() and result > self.obtained_value) or (initial_node.is_min() and result < self.obtained_value):
                 self.obtained_value = result
                 self.obtained_successor = curr_succ
